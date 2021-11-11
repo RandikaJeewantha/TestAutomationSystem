80ddd1c58cad29bd720f92c6467cf8ed74194b9e
Randika Jeewantha <randikaherath0@gmail.com>
2021-11-11 15:15:09 +0530

    Update DashboardPage.java

--- src/test/java/pages/dashboard/DashboardPage.java
+++ src/test/java/pages/dashboard/DashboardPage.java
@@ -6,13 +6,9 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 
 import base.TestBase;
-import pages.bestWeb.BestWebPage;
-import pages.myAccount.MyAccountDomainInterestPage;
 import pages.myAccount.MyAccountEditProfilePage;
 import pages.myAccount.MyAccountMyDocumentsProfilePage;
 import pages.myAccount.MyAccountMyPaymentsPage;
-import pages.myAccount.MyDomainListPage;
-import pages.mysiteService.MysiteServiceDomainSelectPage;
 
 public class DashboardPage extends TestBase {
 
@@ -22,26 +18,14 @@ public class DashboardPage extends TestBase {
 	@FindBy(id = "Form_FillEssentialDetailForm")
 	WebElement fillEssentialDetailForm;
 	
-	@FindBy(xpath = "/html/body/section/div[2]/div[1]/div/div[2]/p[2]/a")
-	WebElement myDomainsButton;
-	
 	@FindBy(xpath = "/html/body/section/div[2]/div[2]/div/div[2]/p[2]/a")
 	WebElement myProfileButton;
 
-	@FindBy(xpath = "/html/body/section/div[2]/div[3]/div/div[2]/p[2]/a")
-	WebElement mySiteButton;
-	
 	@FindBy(xpath = "/html/body/section/div[2]/div[4]/div/div[2]/p[2]/a")
 	WebElement myPaymentsButton;
 	
 	@FindBy(xpath = "/html/body/section/div[2]/div[5]/div/div[2]/p[2]/a")
 	WebElement documentsButton;
-	
-	@FindBy(xpath = "/html/body/section/div[2]/div[6]/div/div[2]/p[2]/a")
-	WebElement registerNowButton;
-	
-	@FindBy(xpath = "/html/body/section/div[2]/div[7]/div/div[2]/p[2]/a")
-	WebElement submitInterestButton;
 	
 	
 	/* Initialising the page objects */
@@ -64,19 +48,9 @@ public class DashboardPage extends TestBase {
 		fillEssentialDetailForm.isDisplayed();
 	}
 	
-	public MyDomainListPage clickMyDomainsButton(){
-		myDomainsButton.click();
-		return new MyDomainListPage();
-	}
-	
 	public MyAccountEditProfilePage clickMyProfileButton(){
 		myProfileButton.click();
 		return new MyAccountEditProfilePage();
-	}
-	
-	public MysiteServiceDomainSelectPage clickMySiteButton(){
-		mySiteButton.click();
-		return new MysiteServiceDomainSelectPage();
 	}
 	
 	public MyAccountMyPaymentsPage clickMyPaymentsButton(){
@@ -87,15 +61,5 @@ public class DashboardPage extends TestBase {
 	public MyAccountMyDocumentsProfilePage clickDocumentsButton(){
 		documentsButton.click();
 		return new MyAccountMyDocumentsProfilePage();
-	}
-	
-	public BestWebPage clickRegisterNowButton(){
-		registerNowButton.click();
-		return new BestWebPage();
-	}
-	
-	public MyAccountDomainInterestPage clickSubmitInterestButton(){
-		submitInterestButton.click();
-		return new MyAccountDomainInterestPage();
 	}
 }
